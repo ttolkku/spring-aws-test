bf263439913a70a5fddf6d265e6feed150995101
ttolkku <cgeunsang@gmail.com>
2022-11-25 18:08:16 +0900

    first commit

---
+++ src/main/java/com/jojoldu/book/springawstest/SpringAwsTestApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springawstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/springawstest/SpringAwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springawstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
