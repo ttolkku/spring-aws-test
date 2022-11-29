f7d913549cb6879a769a4a651df2c4ea17eaccf0
ttolkku <cgeunsang@gmail.com>
2022-11-29 21:17:09 +0900

    ignore update

--- src/main/java/com/jojoldu/book/springawstest/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springawstest/domain/posts/Posts.java
@@ -24,6 +24,7 @@ public class Posts extends BaseTimeEntity {
 
     private String author;
 
+
     @Builder
     public Posts(String title, String content, String author) {
         this.title = title;
