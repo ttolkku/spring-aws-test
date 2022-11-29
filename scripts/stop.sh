fe2f73ff862cedc8e3bdeecd1d9b5d9f1c5f4f46
ttolkku <cgeunsang@gmail.com>
2022-11-30 01:56:57 +0900

    무중단배포 최종

---
+++ scripts/stop.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=#(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLD_PORT=$(find_idle_port)
+
+echo "> $IDLD_PORT 에서 구동 중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLD_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
