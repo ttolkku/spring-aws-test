396800774065a50a576147aa4d32b010101f9724
ttolkku <cgeunsang@gmail.com>
2022-11-30 02:19:42 +0900

    무중단배포 최종 오타수정2

--- scripts/health.sh
+++ scripts/health.sh
@@ -14,13 +14,13 @@ sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$(curl -s http://localhost:$IDLE_PORT/profile)
+  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
     echo "> Health check 성공"
-    swith_proxy
+    switch_proxy
     break
   else
     echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,13 +4,15 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLD_PORT=$(find_idle_port)
+IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLD_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLD_PORT})
+echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
